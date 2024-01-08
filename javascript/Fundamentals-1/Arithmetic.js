3045eb0d86c74bd5f51828a3529a6a68098d1648
Yihenggoh <yiheng29@gmail.com>
2024-01-08 12:26:25 +0800

    js basic Arithmetic

---
+++ javascript/Fundamentals-1/Arithmetic.js
@@ -0,0 +1,23 @@
+let x = 100 + 50; // x = 150
+let y = 120 - 20; // y = 100
+let z = x + y; // z = 250
+
+
+// incrementing
+let a = 5;
+a++; // increment a by 1
+let b = a; // b = 6
+a--; // decrement a by 1
+let c = a; // a = 5
+
+//exponent
+let d = 5;
+let e = d ** 2;
+// produces the same result as Math.pow(d,e)
+
+// order of precedence
+/* left to right
+parentheses
+multiply/divde
+add/subtract
+*/
