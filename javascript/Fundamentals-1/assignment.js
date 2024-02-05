a08aef04fc4f17799300d1c42fc5caeb5548cf54
Yihenggoh <yiheng29@gmail.com>
2024-02-05 21:42:17 +0800

    javascript fundementals1 assignment

---
+++ javascript/Fundamentals-1/assignment.js
@@ -0,0 +1,20 @@
+// 1
+console.log(1+2)
+
+// 2
+console.log(1+2+3+8+9+0)
+
+// 3
+let number = (4+6+9)/77
+document.write(number)
+
+// 4
+let a = 10
+console.log(a)
+let b = 7 * a
+console.log(b)
+
+// 5 
+const MAX = 57
+let actual = MAX - 13
+let percentage = acutal/MAX
